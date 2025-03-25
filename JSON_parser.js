9b216d686a89630d5cb9745c82d094660543b4e5
kishor <kishor.jha243@gmail.com>
2025-03-26 02:12:30 +0530

    did a opsie

---
+++ JSON_parser.js
@@ -0,0 +1,47 @@
+import puppeteer from "puppeteer";
+import fs from "fs";
+
+// created a instaance o f browser
+const browser = await puppeteer.launch({ headless: true });
+const page = await browser.newPage();
+
+// this take a lot of time all request will go throw this , maybe //
+await page.setRequestInterception(true);
+
+page.on("request", (request) => request.continue());
+
+const Collected_json = [];
+
+page.on("response", async (response) => {
+  const Url = response.url();
+  console.log(Url);
+  if (
+    Url.includes("https://api-partner.spotify.com/pathfinder/v1/query") &&
+    Url.includes("operationName=fetchPlaylist&variables")
+  ) {
+    try {
+      console.log("Found the url we needed");
+      const neededjson = await response.json();
+      Collected_json.push(neededjson);
+    } catch (e) {
+      console.log("something definitely went wrong", e);
+    }
+  }
+});
+
+await page.goto("https://open.spotify.com/playlist/2sOMIgioNPngXojcOuR4tn", {
+  waitUntil: "networkidle2",
+  timeout: 21000,
+});
+const realdeal = JSON.stringify(Collected_json[0]);
+console.log(realdeal);
+
+const formodule = await `module.export DataPlaylist = ${realdeal}`;
+
+fs.writeFileSync("Parses_json_data.js", formodule, "utf8", (e) => {
+  if (e) {
+    console.log("maybe try again , maybe ", e);
+  } else {
+    console.log("made the file");
+  }
+});
